3c081be00bbe53be913beb3eb9532a880fb691c1
yahfiilham <yahfi.ilham@gmail.com>
2022-11-26 03:34:01 +0700

    add config db

--- configs/config.go
+++ configs/config.go
@@ -1,1 +1,53 @@
 package configs
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Config struct {
+	DB *gorm.DB
+}
+
+func NewConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	dbLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Disable color
+		},
+	)
+
+	gormConfig := &gorm.Config{
+		// enhance performance config
+		PrepareStmt:            true,
+		SkipDefaultTransaction: true,
+		Logger:                 dbLogger,
+	}
+
+	dsn := os.Getenv("DB_DSN")
+
+	db, err := gorm.Open(mysql.Open(dsn), gormConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("success connect to database")
+
+	app := &Config{
+		DB: db,
+	}
+
+	return app
+}
--- go.mod
+++ go.mod
@@ -11,7 +11,10 @@ require (
 	github.com/go-openapi/swag v0.22.3
 	github.com/go-openapi/validate v0.22.0
 	github.com/jessevdk/go-flags v1.5.0
+	github.com/joho/godotenv v1.3.0
 	golang.org/x/net v0.2.0
+	gorm.io/driver/mysql v1.4.4
+	gorm.io/gorm v1.24.2
 )
 
 require (
@@ -20,6 +23,9 @@ require (
 	github.com/go-openapi/analysis v0.21.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
